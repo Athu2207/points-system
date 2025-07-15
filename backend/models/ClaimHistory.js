308dc3810306924fdd0fb704c95774552f9e2617
Atharva <atharvalole12@gmail.com>
2025-07-16 01:21:51 +0530

    Removed server.js and kept app.js as main entry point

--- backend/models/ClaimHistory.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require('mongoose');
-
-const claimHistorySchema = new mongoose.Schema({
-  userId: { type: mongoose.Schema.Types.ObjectId, ref: 'User' },
-  points: Number,
-  claimedAt: { type: Date, default: Date.now },
-});
-
-module.exports = mongoose.model('ClaimHistory', claimHistorySchema);
